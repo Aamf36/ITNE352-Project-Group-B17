ccde64c3bc8537333ee2bbfc842ec503856a6895
Aysha <aysha.falamarzi03@gmail.com>
2023-12-23 19:42:36 +0300

    Step 1

--- ServerTest.py
+++ ServerTest.py
@@ -1,2 +1,25 @@
-print ('Hello, the server is on & it is running....')
-Print("This is just a test!")
+import socket
+import requests
+import threading
+import json
+
+#Step 1: Starting the server/ Enter arr_icao
+print ('='*5,'Welcome to our server! Our server is on.','='*5)
+
+Link= 'https://api.aviationstack.com/v1/flights'
+
+flight_icao = input("Please enter the airport code (ICAO code):  ")
+
+flight_para= {
+    'access_key':'521d31a723165e2347ec1f7b0d8fac7c'
+    #Since that the API should retrieve 100 records of flights
+    'limit':100
+    'arr_icao':flight_icao
+}
+
+
+
+
+
+
+
