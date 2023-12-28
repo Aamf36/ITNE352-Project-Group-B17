006b68f12dcd66454365a643a117fa87640b7241
Aysha <aysha.falamarzi03@gmail.com>
2023-12-29 01:32:32 +0300

    Fixed errors & bugs

--- ClientTest.py
+++ ClientTest.py
@@ -1,133 +1,145 @@
 import socket
 import PySimpleGUI as PySimpleGUI
 
-#theme for the windows
-PySimpleGUI.theme("LightBlue6")
+# theme for the windows
+PySimpleGUI.theme('LightBlue6')
 
-#step one : establishing the connection to the server 
-csock= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#exception handling for connection
+# step one: establishing the connection to the server
+server_ip = '127.0.0.2'
+server_port = 55555
 
-csock.connect(('127.0.0.1', 65535))
-#PySimpleGUI.PopupError("Couldn't connect to server")
-#exit()
+try:
+    client_socket.connect((server_ip, server_port))
+    print("Connected to the server.")
+except ConnectionRefusedError:
+    print("Connection refused. Make sure the server is running.")
+    exit()
+except Exception as e:
+    print(f"An error occurred while connecting to the server: {e}")
+    exit()
 
-# sending username  
+# sending username
 username_layout = [
-    [PySimpleGUI.Text("Please enter your username :", font=("Arial Bold", 12))], 
+    [PySimpleGUI.Text("Please enter your username: ", font=("Arial Bold", 12))],
     [PySimpleGUI.Input(key="-INPUT-", font=("Arial Bold", 12))],
-    [PySimpleGUI.Submit()], 
+    [PySimpleGUI.Submit()],
     [PySimpleGUI.Cancel()]
 ]
-# create a window for username 
-window = PySimpleGUI.Window("Group_B17 ", username_layout)
-event, uname = window.read()
-uname= uname[0]
+# create a window for username
+window = PySimpleGUI.Window("Group_B17", username_layout, resizable=True)
+event, values = window.read()
 window.close()
 
-# sending the username to server
-csock.send(uname.encode("utf-8")) 
+uname = values["-INPUT-"]
 
-#create windows for later use 
-def window_creation(title,data):
-    window = PySimpleGUI.Window("Group_B17", username_layout, resizable=True)
+# sending the username to server
+client_socket.send(uname.encode("utf-8"))
+
+# create windows for later use
+def window_creation(title, data):
+    window = PySimpleGUI.Window(title, data, resizable=True)
     event, value = window.read()
     window.close()
 
-#step two:  prompt user to choose one of the options
+# step two: prompt user to choose one of the options
 while True:
-    options_layout =[
-            [PySimpleGUI.Text("Choose one of the following options : ")],
-            [PySimpleGUI.Text("1. Show arrived flights" )],
-            [PySimpleGUI.Text("2. Show delayed flights" )],
-            [PySimpleGUI.Text("3. Show all the flights coming from a specific city" )],
-            [PySimpleGUI.Text("4. Show details of a particular flight" )],
-            [PySimpleGUI.Text("5. Quit" )]
-            [PySimpleGUI.Input()],
-            [PySimpleGUI.Button("submit")]
-        ]
-    #create window for options
-    window= PySimpleGUI.Window("Group_B17",options_layout,resizable=True)
+    options_layout = [
+        [PySimpleGUI.Text("Choose one of the following options: ")],
+        [PySimpleGUI.Text("1. Show arrived flights")],
+        [PySimpleGUI.Text("2. Show delayed flights")],
+        [PySimpleGUI.Text("3. Show all the flights coming from a specific city")],
+        [PySimpleGUI.Text("4. Show details of a particular flight")],
+        [PySimpleGUI.Text("5. Quit")],
+        [PySimpleGUI.Input()],
+        [PySimpleGUI.Button("Submit")]
+    ]
+    # create window for options
+    window = PySimpleGUI.Window("Group_B17", options_layout, resizable=True)
     event, option = window.read()
-    option= option[0]
+    option = option[0]
     window.close()
 
-    # send the chosen request to the server 
-    csock.send(option.encode("utf-8"))
+    # send the chosen request to the server
+    client_socket.send(option.encode("utf-8"))
 
-    # if client chooses invalid option 
-    if option != "1" or option != "2" or option != "3" or option != "4" or option != "5":
-     print(" Invalid ! please enter a valid option ")
-     
-    #if client chooses one of the valid options
+    # if client chooses an invalid option
+    if option not in ["1", "2", "3", "4", "5"]:
+        print("Invalid! Please enter a valid option.")
+
+    # if client chooses one of the valid options
     if option == "1":
-        # decode the recv data from the server 
-        data = csock.recv(20480).decode("utf-8")
-        layout= [
-            [PySimpleGUI.Text("All the arrived flights: ",font="Arial")],
-            [PySimpleGUI.Column([[PySimpleGUI.Text(data)]],scrollable=True, vertical_scroll_only=True)]
+        # decode the received data from the server
+        data = client_socket.recv(20480).decode("utf-8")
+        layout = [
+            [PySimpleGUI.Text("All the arrived flights:", font="Arial")],
+            [PySimpleGUI.Column([[PySimpleGUI.Text(data)]], scrollable=True, vertical_scroll_only=True)]
         ]
-        #window creation
-        window_creation("Group_B17",layout)
+        # window creation
+        window_creation("Group_B17", layout)
 
     elif option == "2":
-        # decode the recv data from the server 
-        data = csock.recv(20480).decode("utf-8")
-        layout= [
-            [PySimpleGUI.Text("All the delayed flights: ",font="Arial")],
-            [PySimpleGUI.Column([[PySimpleGUI.Text(data)]],scrollable=True, vertical_scroll_only=True)]
+        # decode the received data from the server
+        data = client_socket.recv(20480).decode("utf-8")
+        layout = [
+            [PySimpleGUI.Text("All the delayed flights:", font="Arial")],
+            [PySimpleGUI.Column([[PySimpleGUI.Text(data)]], scrollable=True, vertical_scroll_only=True)]
         ]
-        #window creation
-        window_creation("Group_B17",layout)
+        # window creation
+        window_creation("Group_B17", layout)
 
     elif option == "3":
-        #prompt user to enter icao code 
+        # prompt user to enter ICAO code
         layout = [
-            [PySimpleGUI.Text("Enter city ICAO:  ",font=("Arial",10))],
+            [PySimpleGUI.Text("Enter city ICAO:  ", font=("Arial", 10))],
             [PySimpleGUI.Input(key="-INPUT-")],
             [PySimpleGUI.Button("Send")]
         ]
-        # window creation 
-        window = PySimpleGUI.Window("Group_B17",layout,resizable=True)
-        event,city= window.read()
-        city=city[0]
+        # window creation
+        window = PySimpleGUI.Window("Group_B17", layout, resizable=True)
+        event, city = window.read()
+        city = city["-INPUT-"]
         window.close()
-    
-        # send ICAO to the server 
-        csock.send(city.encode("utf-8"))
-        # recv flight data
-        flight_data = csock.recv(20480).decode("utf-8")
+
+        # send ICAO to the server
+        client_socket.send(city.encode("utf-8"))
+        # receive flight data
+        flight_data = client_socket.recv(20480).decode("utf-8")
         flight_layout = [
-            [PySimpleGUI.Text("All the flights coming from "+city.capitalize())],
-            [PySimpleGUI.Column([[PySimpleGUI.Text(flight_data)]],scrollable=True, vertical_scroll_only=True)]]
-        #window creation 
-        window_creation("Group_B17",flight_layout)
+            [PySimpleGUI.Text("All the flights coming from " + city.capitalize())],
+            [PySimpleGUI.Column([[PySimpleGUI.Text(flight_data)]], scrollable=True, vertical_scroll_only=True)]
+        ]
+        # window creation
+        window_creation("Group_B17", flight_layout)
 
     elif option == "4":
         flight_layout = [
-            [PySimpleGUI.Text("Enter city ICAO or flight number: ",font=("Arial",12))],
+            [PySimpleGUI.Text("Enter city ICAO or flight number: ", font=("Arial", 12))],
             [PySimpleGUI.Input()],
-            [PySimpleGUI.Button("send")]]
-        
-        # create window  
+            [PySimpleGUI.Button("Send")]
+        ]
+
+        # create window
         window = PySimpleGUI.Window("Group_B17", flight_layout, resizable=True)
         event, flight = window.read()
-        flight= flight[0]
+        flight = flight[0]
         window.close()
 
-        # send icao to the server 
-        csock.send(flight.encode("utf-8"))
-        # recv data from the server
-        flight_data = csock.recv(20480).decode("utf-8")
-        layout=[ 
-            [PySimpleGUI.Text(f"Details of flight with this city ICAO/flight number: {flight.capitalize()}")]]
-        #window creation 
-        window_creation("Group_B17",layout)
-# step three :closing the connection if user chooses quit 
-    elif option == "5":
-        csock.send((uname + " is disconnected").encode("utf-8"))
-        csock.close()
-        break         
+        # send ICAO to the server
+        client_socket.send(flight.encode("utf-8"))
+        # receive data from the server
+        flight_data = client_socket.recv(20480).decode("utf-8")
+        layout = [
+            [PySimpleGUI.Text(f"Details of flight with this city ICAO/flight number: {flight.capitalize()}")],
+            [PySimpleGUI.Column([[PySimpleGUI.Text(flight_data)]], scrollable=True, vertical_scroll_only=True)]
+        ]
+        # window creation
+        window_creation("Group_B17", layout)
 
+    # step three: closing the connection if the user chooses to quit
+    elif option == "5":
+        client_socket.send((uname + " is disconnected").encode("utf-8"))
+        client_socket.close()
+        break
 
--- ServerTest.py
+++ ServerTest.py
@@ -3,56 +3,59 @@ import requests
 import threading
 import json
 
-#Step 1: Starting the server/ Enter arr_icao
-print ('='*5,'Welcome to our server! Our server is on.','='*5)
+# Step 1: Starting the server/ Enter arr_icao
+print('=' * 5, 'Welcome to our server! Our server is on.', '=' * 5)
 
-flight_icao = input("Please enter the airport code (ICAO code):  ")
+flight_icao = input("Please enter the airport code (ICAO code): ")
 
-params= {
-    #Step 2: Since that the API should retrieve 100 records of flights
-    'limit':100,
-    'arr_icao':flight_icao
+params = {
+    # Step 2: Since that the API should retrieve 100 records of flights
+    'limit': 100,
+    'arr_icao': flight_icao
 }
 
-try: #Error handling and exception handling
-    with open("G_B17Test.json","w") as file:
-        #Get request to the specidfied API
-        response = requests.get('http://api.aviationstack.com/v1/flights?access_key=521d31a723165e2347ec1f7b0d8fac7c', params)
-        
-        if response.status_code!=200:
-            state="Request is not done successfully! Server failure took place."
-        else:
-            state="Request is done successfully!"
-    #dump function is used to push the incoming data from the API to the file
-    json.dump(response.json(),file, indent=4)
-    print("Retrieved data is added successfully!")
-except Exception:
-    print("Error!, failled to retreive data!")
+with open("G_B17Test.json", "w") as file:
+    response = requests.get('http://api.aviationstack.com/v1/flights?access_key=521d31a723165e2347ec1f7b0d8fac7c',
+                            params)
 
-#Step 4: handling connections between the server and the clients' requests
-def connect(socket,address,thread_no,clients):
-    print('+'*5,' Thread: ',thread_no,' is ready to receive the username from the client ', '+'*5)
+    if response.status_code != 200:
+        state = "Request is not done successfully! Server failure took place."
+    else:
+        state = "Request is done successfully!"
 
-#Error handling and exception handling
+    print(state)
+
+    if state == "Request is done successfully!":
+        json.dump(response.json(), file, indent=4)
+        print("Retrieved data is added successfully!")
+    else:
+        print("Error!, failed to retrieve data!")
+
+
+# Step 4: handling connections between the server and the clients' requests
+def connect(socket, address, thread_no, clients):
+    print('+' * 5, ' Thread: ', thread_no, ' is ready to receive the username from the client ', '+' * 5)
+
+    # Error handling and exception handling
     try:
-        # receive the client's username from the socket 
+        # receive the client's username from the socket
         username = socket.recv(2048).decode('utf-8')
     except ConnectionResetError:
-        print('The connection with ', address,' is currently closed')
+        print('The connection with ', address, ' is currently closed')
         return
 
     print(username, 'is connected to the server')
     # add the username of the client to the list defined inorder to return the currently connected clients
     clients.append(username)
     print('Currently connected clients are: ', clients)
-    
-    #Listening for requests 
+
+    # Listening for requests
     while True:
-        CityFound=False
-        FlightNoFound=False
-        counter=0 #Inorder to count the number of flights
+        CityFound = False
+        FlightNoFound = False
+        counter = 0  # Inorder to count the number of flights
         try:
-            # receiving requests' No. from the client 
+            # receiving requests' No. from the client
             option = socket.recv(2048).decode('utf-8')
             if option == '1':
                 print(username, 'chose the option number', option, ": All Arrived Flights")
@@ -67,128 +70,141 @@ def connect(socket,address,thread_no,clients):
             clients.remove(username)
             socket.close()
             return
-        #Opening the json file, inorder to read
-        with open('G_B17_Test.json', 'r') as file:
-            #Retrieving data
+        # Opening the json file, inorder to read
+        with open('G_B17Test.json', 'r') as file:
+            # Retrieving data
             data = json.load(file)
 
         if option == '1':
             print("\n -----All Arrived Flights----- \n")
-            info=''
+            info = ''
             for flight in data['data']:
-                if flight['flight_status']=='landed':
-                    counter+=1
-                    info += " \n Flight:",counter
-                    info += "\n Flight IATA Code:",(str(flight['flight']['iata']))
-                    info += "\n Departure Airpot:",(str(flight['departure']['airport']))
-                    info += "\n Arrival Time Scheduled:",(str(flight['arrival']['scheduled']))
-                    info += "\n Arrival Terminal:",(str(flight['arrival']['terminal']))
-                    info += "\n Arrival Gate:",(str(flight['arrival']['gate']))
-                    info += "\n","-"*20
+                if flight['flight_status'] == 'landed':
+                    counter += 1
+                    info += " \n Flight:", counter
+                    info += "\n Flight IATA Code:", str(flight['flight']['iata'])
+                    info += "\n Departure Airpot:", str(flight['departure']['airport'])
+                    info += "\n Arrival Time Scheduled:", str(flight['arrival']['scheduled'])
+                    info += "\n Arrival Terminal:", str(flight['arrival']['terminal'])
+                    info += "\n Arrival Gate:", str(flight['arrival']['gate'])
+                    info += "\n", "-" * 20
                 if not info:
-                    info='No flights has arrived'
+                    info = 'No flights has arrived'
                 socket.send(info.encode('utf-8'))
-                
+
         elif option == '2':
             print("\n -----All Delayed Flights----- \n")
-            info=''
+            info = ''
             for flight in data['data']:
-                if flight['arrival']['delay']!= None:
-                    counter+=1
-                    info += " \n Flight:",counter
-                    info += "\n Flight IATA Code:",(str(flight['flight']['iata']))
-                    info += "\n Departure Airpot:",(str(flight['departure']['airport']))
-                    info += "\n Original Departure Time Scheduled:",(str(flight['arrival']['scheduled'])) #Or departure estimated?
-                    info += "\n Estimated Arrival Time:",(str(flight['arrival']['estimated']))
-                    info += "\n Arrival Terminal:",(str(flight['arrival']['terminal']))
-                    info += "\n Delay:",(str(flight['arrival']['delay']))
-                    info += "\n Arrival Gate:",(str(flight['arrival']['gate']))
-                    info += "\n","-"*20
+                if flight['arrival']['delay'] != None:
+                    counter += 1
+                    info += " \n Flight:", counter
+                    info += "\n Flight IATA Code:", str(flight['flight']['iata'])
+                    info += "\n Departure Airpot:", str(flight['departure']['airport'])
+                    info += "\n Original Departure Time Scheduled:", str(flight['arrival']['scheduled'])  # Or departure estimated?
+                    info += "\n Estimated Arrival Time:", str(flight['arrival']['estimated'])
+                    info += "\n Arrival Terminal:", str(fate['terminal'])
+                    info += "\n Arrival Gate:", str(flight['arrival']['gate'])
+                    info += "\n Delay:", str(flight['arrival']['delay'])
+                    info += "\n", "-" * 20
                 if not info:
-                    info='No delayed flights has been found'
+                    info = 'No delayed flights found'
                 socket.send(info.encode('utf-8'))
 
-                
         elif option == '3':
-            city=socket.recv(2048).decode('utf-8')
-            print("\n -----All Flights Coming from Specific City----- \n")
-            info=''
+            print("\n -----All Flights Coming from a Specific City----- \n")
+            try:
+                # receiving the city name from the client
+                city = socket.recv(2048).decode('utf-8')
+                print("The client ", username, "wants to check all flights coming from ", city)
+            except ConnectionResetError:
+                print(username, ' is disconnected')
+                clients.remove(username)
+                socket.close()
+                return
+
+            info = ''
             for flight in data['data']:
-                if city in flight['departure']['airport']:
-                    CityFound=True
-                    counter+=1
-                    info += " \n Flight:",counter
-                    info += "\n Flight IATA Code:",(str(flight['flight']['iata']))
-                    info += "\n Departure Airpot:",(str(flight['departure']['airport']))
-                    info += "\n Original Departure Time Scheduled:",(str(flight['arrival']['scheduled'])) #departure actual or estimated?
-                    info += "\n Estimated Arrival Time:",(str(flight['arrival']['estimated']))
-                    info += "\n Arrival Gate:",(str(flight['arrival']['gate']))
-                    info += "\n Departure Gate:",(str(flight['departure']['gate']))
-                    info += "\n Flight Status:",(str(flight['flight_status']))
-                    info += "\n","-"*20
-                if not CityFound:
-                    info+='No flights has been found for the specified city'
-                socket.send(info.encode('utf-8'))           
+                if flight['departure']['airport'] == city:
+                    counter += 1
+                    info += " \n Flight:", counter
+                    info += "\n Flight IATA Code:", str(flight['flight']['iata'])
+                    info += "\n Departure Airpot:", str(flight['departure']['airport'])
+                    info += "\n Arrival Time Scheduled:", str(flight['arrival']['scheduled'])
+                    info += "\n Arrival Terminal:", str(flight['arrival']['terminal'])
+                    info += "\n Arrival Gate:", str(flight['arrival']['gate'])
+                    info += "\n", "-" * 20
+                    CityFound = True
+            if not CityFound:
+                info = 'No flights coming from the specified city'
+            socket.send(info.encode('utf-8'))
 
         elif option == '4':
-            FlightNO=socket.recv(2048).decode('utf-8')
-            print('\n -----Details of Flight Number:',FlightNO,'----- \n')
-            info=''
+            print("\n -----Details of a Particular Flight----- \n")
+            try:
+                # receiving the flight number from the client
+                flight_no = socket.recv(2048).decode('utf-8')
+                print("The client ", username, "wants to check the details of flight number ", flight_no)
+            except ConnectionResetError:
+                print(username, ' is disconnected')
+                clients.remove(username)
+                socket.close()
+                return
+
+            info = ''
             for flight in data['data']:
-                if flight['flight']['number']== FlightNO:
-                    FlightNoFound=True
-                    info += " \n Flight Number:",FlightNO
-                    info += "\n Flight IATA Code:",(str(flight['flight']['iata']))
-                    info += "\n Flight Date:",(str(flight['flight_date']))
-                    info += "\n Departure Airpot:",(str(flight['departure']['airport']))
-                    info += "\n Departure Gate:",(str(flight['departure']['gate']))
-                    info += "\n Arrival Airport:",(str(flight["arrival"]["airport"]))
-                    info += "\n Arrival Gate:",(str(flight['arrival']['gate']))
-                    info += "\n Arrival Terminal:",(str(flight['arrival']['terminal']))
-                    info += "\n Flight Status:",(str(flight['flight_status']))
-                    info += "\n Scheduled Departure Time:",(str(flight['departure']['scheduled']))
-                    info += "\n Scheduled Arrival Time:",(str(flight['arrival']['scheduled']))
-                    info += "\n","-"*20
-                    #break
-                if not FlightNoFound:
-                    info+='No flight has been found for the flight number specified'
-                socket.send(info.encode('utf-8'))           
+                if flight['flight']['iata'] == flight_no:
+                    counter += 1
+                    info += "\n Flight:", counter
+                    info += "\n Flight IATA Code:", str(flight['flight']['iata'])
+                    info += "\n Departure Airpot:", str(flight['departure']['airport'])
+                    info += "\n Arrival Time Scheduled:", str(flight['arrival']['scheduled'])
+                    info += "\n Arrival Terminal:", str(flight['arrival']['terminal'])
+                    info += "\n Arrival Gate:", str(flight['arrival']['gate'])
+                    info += "\n", "-" * 20
+                    FlightNoFound = True
+            if not FlightNoFound:
+                info = 'No flight found with the specified flight number'
+            socket.send(info.encode('utf-8'))
 
         elif option == '5':
-            print(username, 'is being disconnected') 
+            # Close the connection with the client
+            print('The client ', username, 'is disconnected')
             clients.remove(username)
-            print('Here are the connected clients:',clients)
             socket.close()
             return
 
-        # Incase an invalid option is chosen
-        else:
-            print('Invalid request from', username, 'so the connection is closing')
-            clients.remove(username)
-            print('Currently connected clients are:',clients)
-            socket.close()
-            return
 
-# Creating the server socket and accepting incoming connections
+# Step 3: creating threads to handle the clients' requests
+clients = []
+threads = []
+thread_no = 0
+
+# Create a socket object
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# bind the socket to IP address
-server_socket.bind(('127.0.0.1', 65535))
-# listen for incoming connections
+
+# Define the host and port
+host = '127.0.0.2'
+port = 55555
+
+# Bind the socket to a specific address and port
+server_socket.bind((host, port))
+
+# Listen for incoming connections
 server_socket.listen()
 
-clients = []  # List to keep track of connected clients
+print('Waiting for incoming connections...')
 
-# Function to handle each client connection
-def handle_client_connection(client_socket, address):
-    # Your existing code for handling the client connection
-
-# Accept incoming connections and create threads to handle each connection
- while True:
+while True:
+    # Accept a new connection
     client_socket, address = server_socket.accept()
-    print('Accepted connection from:', address)
-    # Create a new thread to handle the client connection
-    client_thread = threading.Thread(target=handle_client_connection, args=(client_socket, address))
-    client_thread.start()
+    thread_no += 1
+
+    # Create and start a new thread to handle the client connection
+    thread = threading.Thread(target=connect, args=(client_socket, address, thread_no, clients))
+    thread.start()
+
+    threads.append(thread)
 
 
 
